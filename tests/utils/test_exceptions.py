62dd8bef441633ce8cc300b7956c2cf906bb068d
Jake Wells <38321395+madebyjake@users.noreply.github.com>
2025-01-26 20:27:42 -0500

    tests(utils): add tests for custom exceptions utility

---
+++ tests/utils/test_exceptions.py
@@ -0,0 +1,23 @@
+"""Tests for custom exceptions."""
+
+import pytest
+
+from hashreport.utils.exceptions import (
+    EmailError,
+    FileAccessError,
+    HashReportError,
+    ReportError,
+    ValidationError,
+)
+
+
+def test_hashreport_error_inheritance():
+    """Test that derived exceptions inherit from HashReportError."""
+    if not issubclass(FileAccessError, HashReportError):
+        pytest.fail("FileAccessError should inherit from HashReportError")
+    if not issubclass(ReportError, HashReportError):
+        pytest.fail("ReportError should inherit from HashReportError")
+    if not issubclass(EmailError, HashReportError):
+        pytest.fail("EmailError should inherit from HashReportError")
+    if not issubclass(ValidationError, HashReportError):
+        pytest.fail("ValidationError should inherit from HashReportError")
